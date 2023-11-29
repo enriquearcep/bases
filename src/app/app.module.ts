c98b2c975b49984d19d80b16ec05a5ae4c03d793
Enrique <enriquearce00@gmail.com>
2023-11-28 21:01:51 -0600

    Counter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CounterComponent } from './counter/counter.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,7 +7,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
